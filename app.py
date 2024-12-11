b1c0e463146f969a533656315806470903643340
masterIoT <157422488+masterIoT@users.noreply.github.com>
2024-12-11 23:01:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def check_service(url):
         return jsonify({"status": "DOWN", "error": str(e)}), 503
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
